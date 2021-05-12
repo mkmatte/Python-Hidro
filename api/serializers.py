f79814e095568d18158060929568c79e52cb2918
Marcelo Matte <marcelokuchar@gmail.com>
2021-05-12 14:12:19 -0400

    Commit 2

--- api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from data.models import *
-from monitor.models import *
-
-
-# class DataSensorSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = DataSensor
-#         fields = ['id', 'id_controller', 'id_sensor', 'data', 'data_type', 'date_created']
