b39440cc969f37aa254613ba3bcec7b670292439
mkmatte <marcelokuchar@gmail.com>
2022-02-08 17:33:35 -0400

    melhorias na interface html

--- control/views.py
+++ control/views.py
@@ -14,36 +14,50 @@ class form_Project(forms.Form):
 
 def data_sensor(request):
     graphs = []
+    dados=[]
+    tag_sensor = []
+    all_dados = []
     sensor = Sensor.objects.all()
     for s in sensor:
         try:
             qs = DataSensor.objects.filter(sensor=s.id).order_by('-id')[:30]
         except:
             qs = DataSensor.objects.filter(sensor=s.id)
+        dados.append(qs)
         list_of_datetimes = [x.date_created for x in qs]
         x = pd.to_datetime(list_of_datetimes, format="%d-%m %H:%M")
         y = [float(y.value) for y in qs]
         tag_plot = s.tag_sensor +' : '+s.controller.label_controller +' ('+ s.controller.sector.project.name_project +')'
         chart = get_plot(x,y,tag_plot,qs[0].data_type)
-        graphs.append(chart)
-    return render(request,"main/data_sensor.html",{'graphs':graphs})
+        graphs.append(chart)        
+        q = qs[0]
+        tag_sensor.append(q.sensor.tag_sensor)
+        all_dados.append(zip(graphs,dados,tag_sensor))
+    return render(request,"main/data_sensor.html",{'all_dados':all_dados})
 
 
 def data_actuator(request):
     graphs = []
+    dados=[]
+    tag_actuator = []
+    all_dados = []
     actuator = Actuator.objects.all()
     for s in actuator:
         try:
             qs = DataActuador.objects.filter(actuator=s.id).order_by('-id')[:20]
         except:
             qs = DataActuador.objects.filter(actuator=s.id)
+        dados.append(qs)
         list_of_datetimes = [x.date_created for x in qs]
         x = pd.to_datetime(list_of_datetimes, format="%d-%m %H:%M")
         y = [float(y.value) for y in qs]
         tag_plot = s.tag_actuator +' : '+s.controller.label_controller +' ('+ s.controller.sector.project.name_project +')'
         chart = get_plot(x,y,tag_plot,qs[0].data_type)
         graphs.append(chart)
-    return render(request,"main/data_actuator.html",{'graphs':graphs})    
+        q = qs[0]
+        tag_actuator.append(q.actuator.tag_actuator)
+        all_dados.append(zip(graphs,dados,tag_actuator))
+    return render(request,"main/data_actuator.html",{'all_dados':all_dados})    
     
     
     
